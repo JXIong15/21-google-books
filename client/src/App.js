29d66bff215e4c7ce369ddef1d00b5b2936626c5
Pikachu <jouxiong15@gmail.com>
2021-07-04 01:14:25 -0500

    route works

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+
+          <Route exact path="/search" component={Search} />
+
+          <Route exact path="/saved" component={Saved} />
+
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
+
+
+    // <div className="App">
+    //   <div className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h2>Welcome to React</h2>
+    //   </div>
+    //   <p className="App-intro">
+    //     To get started, edit <code>src/App.js</code> and save to reload.
+    //   </p>
+    // </div>
   );
 }
 
