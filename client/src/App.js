2291e0917e86ff41eff40444adf03778980b7c2d
Pikachu <jouxiong15@gmail.com>
2021-07-05 21:19:08 -0500

    added header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
-import Search from "./pages/Search";
-import Saved from "./pages/Saved";
-import Home from "./pages/Home";
-import Nav from "./components/Nav";
+import React from "react";
+import Search from "./components/pages/Search";
+import Saved from "./components/pages/Saved";
+import Home from "./components/pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -11,26 +12,15 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/saved" component={Saved} />
-
         </Switch>
+        <Footer />
       </div>
     </Router>
-
-
-    // <div className="App">
-    //   <div className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h2>Welcome to React</h2>
-    //   </div>
-    //   <p className="App-intro">
-    //     To get started, edit <code>src/App.js</code> and save to reload.
-    //   </p>
-    // </div>
   );
 }
 
---
+++ client/src/components/pages/Search.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Search() {
+    return (
+        <section>
+            <div className="search-form">
+            <h3>Book Search</h3>
+
+            </div>
+
+            <div className="search-results">
+                
+            </div>
+        </section>
+    );
+}
+
+export default Search;
