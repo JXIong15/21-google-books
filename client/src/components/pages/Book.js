8b5484888dc80584261985c0bbde9a4ea7b020c5
Pikachu <jouxiong15@gmail.com>
2021-07-05 23:53:44 -0500

    got books to display

--- client/src/components/pages/Book.js
+++ client/src/components/pages/Book.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Book() {
-  return (
-    <div>
-        <h1>BOOK</h1>
-    </div>
-  );
+function Book(props) {
+    console.log(props)
+    return (
+        <div>
+            <div>
+                <h3>{props.title}</h3>
+                <a href={props.link} target="_blank" rel="noopener noreferrer">
+                    <button>View</button>
+                </a>
+                <button>Save</button>
+            </div>
+            <h4>Written By: {props.authors}</h4>
+            <section>
+                <img src={props.img} alt={props.title + " thumbnail"}></img>
+                <p>{props.desc}</p>
+            </section>
+        </div>
+    );
 }
 
 export default Book;
--- client/src/components/pages/Results.js
+++ client/src/components/pages/Results.js
@@ -2,16 +2,7 @@ import React from "react";
 import Book from "./Book";
 
 function Results(props) {
-    // console.log(props.resultsList);
-    // console.log(typeof props.resultsList)
     let resList = Object.values(props.resultsList);
-    
-    // console.log(resList)
-    // console.log(typeof resList)
-    
-        
-    // console.log(typeof resList)
-
     return (
         <div className="search-res">
             <h3>Results</h3>
@@ -21,8 +12,8 @@ function Results(props) {
                             key={book.id}
                             title={book.volumeInfo.title}
                             authors={book.volumeInfo.authors}
-                            description={book.volumeInfo.description}
-                            image={book.volumeInfo.imageLinks.thumbnail}
+                            desc={book.volumeInfo.description}
+                            img={book.volumeInfo.imageLinks.thumbnail}
                             link={book.volumeInfo.previewLink}
                         />
                     )
